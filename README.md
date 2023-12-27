114ec48472a148c7678be5f3e8d4789105aba47f
Landon <landonyoung71@gmail.com>
2023-12-27 18:40:53 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Description - Uses Python's os and zipfile modules to copy the entire contents of a folder and create a ZIP file backup. If the name of the ZIP file already exists, it increments the number in the ZIP file's filename.
+
+Instructions - User must import the os and zipfile modules from Python. From there, the user can copy the path of the folder they would like to make a backup of and pass it into the function as an argument. 
---
+++ backupToZip.py
@@ -0,0 +1,34 @@
+#! python3
+
+# copies an entire folder and its contents into
+# a zip file whose filename increments.
+
+import zipfile, os
+
+
+def backupToZip(folder):
+    folder = os.path.abspath(folder)
+
+    number = 1
+    while True:
+        zip_filename = os.path.basename(folder) + '_' + str(number) + '.zip'
+        if not os.path.exists(zip_filename):
+            break
+        number = number + 1
+
+    print(f'Creating {zip_filename}...')
+
+    backup_zip = zipfile.ZipFile(zip_filename, 'w')
+
+    for foldername, subfolders, filenames in os.walk(folder):
+        print(f'Adding files in {foldername}...')
+        backup_zip.write(foldername)
+        for filename in filenames:
+            new_base = os.path.basename(folder) + '_'
+            if filename.startswith(new_base) and filename.endswith('.zip'):
+                continue
+            backup_zip.write(os.path.join(foldername, filename))
+    backup_zip.close()
+    print('Done.')
+
+backupToZip('/Users/landonyoung/Programming/fun')
